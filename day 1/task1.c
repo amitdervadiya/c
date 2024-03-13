338f4d325ce4efeff6b84cf2538967b8f1d61c4c
amitdervadiya <amitdervadiya@gmail.com>
2024-03-13 19:20:40 +0530

    all work

---
+++ day 1/task1.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+void main(){
+
+
+    printf("my name is : dervadiya amit\n");
+     printf("my age is : 18\n");
+     printf("my school name is : shree harikrishna school");
+
+
+}
