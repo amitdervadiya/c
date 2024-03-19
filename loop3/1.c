25d70dca8b066c407d6e25cecdbe383dec4b0e79
amitdervadiya <amitdervadiya@gmail.com>
2024-03-19 19:22:23 +0530

    loop3

---
+++ loop3/1.c
@@ -0,0 +1,17 @@
+
+#include <stdio.h>
+void main(){
+
+    int n,sum=0;
+    printf("Enter any number:");
+    scanf("%d",&n);
+
+    for (int  i = 1; i<=n;i++)
+    {
+        sum+=i;                                                              ;
+       
+    }
+     printf("The sum of all numbers: %d",sum);
+    
+
+}
